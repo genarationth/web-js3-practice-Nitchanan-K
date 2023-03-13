1d3b479e8550e5618566a9b99df275de861a96c8
Nitchanan - K <108390376+Nitchanan-K@users.noreply.github.com>
2023-03-13 09:16:22 +0000

    first commit

---
+++ test.js
@@ -0,0 +1,22 @@
+  // Addition function
+  function add(a, b) {
+    return a + b;
+  }
+  
+  // Subtraction function
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Multiplication function
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Division function
+  function divide(a, b) {
+    if (b === 0) {
+      return "Error: Division by zero";
+    }
+    return a / b;
+  }
